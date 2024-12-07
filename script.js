e0cf6e9da767d56e9b6eb0b62c893d9fda8a80a6
karinutza <167879974+karinaaaaaaaaaaaaa@users.noreply.github.com>
2024-12-07 14:50:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openacasa(){
+    window.location.href="index.html";
+}
+
+function openarticole(){
+    window.location.href="articole.html";
+}
+
+function openpoze(){
+    window.location.href="poze.html";
+}
